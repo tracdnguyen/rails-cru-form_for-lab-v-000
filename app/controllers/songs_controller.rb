4b111ee131603efddda79fc30d3ce466a77225d8
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-31 01:10:46 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,29 @@
 class SongsController < ApplicationController
-  def create
+  def index
 
   end
 
   def show
+    @song = Song.find(params[:id])
+  end
 
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(params.require(:song))
+    @song.save
+    redirect_to song_path(@artist)
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song))
+    redirect_to song_path(@song)
   end
 end
