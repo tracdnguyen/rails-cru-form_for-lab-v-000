322210e98de4022af65314449eaa2c95fc85a77e
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-31 01:27:54 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -16,14 +16,14 @@ class GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(params.require(:genre))
+    @genre = Genre.new(params.require(:genre).permit(:name))
     @genre.save
     redirect_to genre_path(@genre)
   end
 
   def update
     @genre = Genre.find(params[:id])
-    @genre.update(params.require(:genre))
+    @genre.update(params.require(:genre).permit(:name))
     redirect_to genre_path(@genre)
   end
 end
